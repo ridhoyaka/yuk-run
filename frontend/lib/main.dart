7357fc1f5c5e236e53d404ba1745f2a373560ff1
Agiza22 <agisafurfaiz@gmail.com>
2026-07-23 21:43:14 +0700

    delete watermark debug in aplication

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'RunNotPace',
       theme: ThemeData(
         brightness: Brightness.dark,
@@ -20,3 +21,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+//192.168.1.12
